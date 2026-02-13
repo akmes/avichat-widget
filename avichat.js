3e99e127ad8857d229a8f4680a1e143fb0f9e228
gabriel silva <gabriel.silva.sergio1@gmail.com>
2026-02-13 11:40:46 -0400

    add svg like and deslike

--- avichat.js
+++ avichat.js
@@ -587,14 +587,23 @@ button {
           const createBtn = (type) => {
             const btn = document.createElement('button');
             btn.innerHTML = type === 'like'
-              ? `<svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                  <path d="M14 9V5a3 3 0 0 0-6 0v4"/>
-                  <path d="M5 15h14l-1 7H6l-1-7z"/>
-                </svg>`
-              : `<svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                  <path d="M10 15v4a3 3 0 0 0 6 0v-4"/>
-                  <path d="M19 9H5l1-7h12l1 7z"/>
-                </svg>`;
+              ? `<svg fill="#000000" xmlns="http://www.w3.org/2000/svg" 
+                    width="800px" height="800px" viewBox="0 0 52 52" enable-background="new 0 0 52 52" xml:space="preserve">
+                  <g>
+                    <path d="M10.5,21h-5C4.7,21,4,21.7,4,22.5v23C4,46.3,4.7,47,5.5,47H8c2.2,0,4-1.8,4-4V22.5C12,21.7,11.3,21,10.5,21
+                      z"/>
+                    <path d="M44,22h-6c-2.2,0-4-1.8-4-4V8c0-2.2-1.8-4-4-4h-2.5C26.7,4,26,4.7,26,5.5v6c0,5.3-3.7,10.5-8.5,10.5
+                      c-0.8,0-1.5,0.7-1.5,1.5v20c0,0.8,0.6,1.5,1.4,1.5c6.8,0.3,9.1,3,16.2,3c7.5,0,14.4-0.8,14.4-9.5v-5V26C48,23.8,46.2,22,44,22z"/>
+                  </g>
+                  </svg>`
+              : `<svg fill="#000000" xmlns="http://www.w3.org/2000/svg" 
+                    width="800px" height="800px" viewBox="0 0 52 52" enable-background="new 0 0 52 52" xml:space="preserve">
+                  <g>
+                    <path d="M12,29.5V9c0-2.2-1.8-4-4-4H5.5C4.7,5,4,5.7,4,6.5v23C4,30.3,4.7,31,5.5,31h5C11.3,31,12,30.3,12,29.5z
+                      M48,26V13.5C48,4.8,41.1,4,33.6,4c-7.1,0-9.4,2.7-16.2,3C16.6,7,16,7.7,16,8.5v20c0,0.8,0.7,1.5,1.5,1.5c4.8,0,8.5,5.2,8.5,10.5v6
+                      c0,0.8,0.7,1.5,1.5,1.5H30c2.2,0,4-1.8,4-4V34c0-2.2,1.8-4,4-4h6C46.2,30,48,28.2,48,26z"/>
+                  </g>
+                  </svg>`;
 
             btn.style.border = '1px solid var(--border-color)';
             btn.style.background = 'white';
